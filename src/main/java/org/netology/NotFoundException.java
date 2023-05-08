d15b99080e2ed36f64156f74aab228bddcbe880c
Kristina Budnik <ssspers92@gmail.com>
2023-05-08 16:06:03 +0300

    first commit

---
+++ src/main/java/org/netology/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.netology;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(int id) {
+        super("Element with id: " + id + " not found");
+    }
+}
---
+++ src/test/java/org/netology/ProductTest.java
@@ -0,0 +1,46 @@
+package org.netology;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+
+    @Test
+    public void TestThereIsProduct() {
+
+        ShopRepository repo = new ShopRepository();
+
+        Product product1 = new Product(1, "Kiwi", 129);
+        Product product2 = new Product(2, "Apple", 78);
+        Product product3 = new Product(3, "Juice", 150);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        repo.remove(2);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestThereIsNotProduct() {
+
+        ShopRepository repo = new ShopRepository();
+
+        Product product1 = new Product(1, "Kiwi", 129);
+        Product product2 = new Product(2, "Apple", 78);
+        Product product3 = new Product(3, "Juice", 150);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> repo.remove(5));
+    }
+
+}
